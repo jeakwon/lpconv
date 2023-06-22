0ee8e6bcae987e7d3376926206e8a90115a6b944
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-06-22 19:00:40 +0900

    git remove cifar10

--- models/cifar100.py
+++ models/cifar100.py
@@ -4,123 +4,121 @@ import torchvision.models as models
 from timm.models.registry import register_model
 from .lpconv import LpConvert
 
-num_classes = 100
-
 # AlexNet
 @register_model
 def alexnet_cifar100(pretrained=False, **kwargs):
-    model = models.alexnet(pretrained=pretrained, num_classes=num_classes)
+    model = models.alexnet(pretrained=pretrained, num_classes=100)
     model.features[0] = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1) # for cifar shape 32x32
     return model
 
 # VGG
 @register_model
 def vgg11_cifar100(pretrained=False, **kwargs):
-    model = models.vgg11(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg11(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def vgg13_cifar100(pretrained=False, **kwargs):
-    model = models.vgg13(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg13(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def vgg16_cifar100(pretrained=False, **kwargs):
-    model = models.vgg16(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg16(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def vgg19_cifar100(pretrained=False, **kwargs):
-    model = models.vgg19(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg19(pretrained=pretrained, num_classes=100)
     return model
 
 # VGG + BN
 @register_model
 def vgg11_bn_cifar100(pretrained=False, **kwargs):
-    model = models.vgg11_bn(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg11_bn(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def vgg13_bn_cifar100(pretrained=False, **kwargs):
-    model = models.vgg13_bn(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg13_bn(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def vgg16_bn_cifar100(pretrained=False, **kwargs):
-    model = models.vgg16_bn(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg16_bn(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def vgg19_bn_cifar100(pretrained=False, **kwargs):
-    model = models.vgg19_bn(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg19_bn(pretrained=pretrained, num_classes=100)
     return model
 
 # ResNet
 @register_model
 def resnet18_cifar100(pretrained=False, **kwargs):
-    model = models.resnet18(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet18(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 @register_model
 def resnet34_cifar100(pretrained=False, **kwargs):
-    model = models.resnet34(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet34(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 @register_model
 def resnet50_cifar100(pretrained=False, **kwargs):
-    model = models.resnet50(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet50(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 @register_model
 def resnet101_cifar100(pretrained=False, **kwargs):
-    model = models.resnet101(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet101(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 @register_model
 def resnet152_cifar100(pretrained=False, **kwargs):
-    model = models.resnet152(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet152(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 # DenseNet
 @register_model
 def densenet121_cifar100(pretrained=False, **kwargs):
-    model = models.densenet121(pretrained=pretrained, num_classes=num_classes)
+    model = models.densenet121(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 @register_model
 def densenet161_cifar100(pretrained=False, **kwargs):
-    model = models.densenet161(pretrained=pretrained, num_classes=num_classes)
+    model = models.densenet161(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 96, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 @register_model
 def densenet169_cifar100(pretrained=False, **kwargs):
-    model = models.densenet169(pretrained=pretrained, num_classes=num_classes)
+    model = models.densenet169(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 @register_model
 def densenet201_cifar100(pretrained=False, **kwargs):
-    model = models.densenet201(pretrained=pretrained, num_classes=num_classes)
+    model = models.densenet201(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 # WideResNet
 @register_model
 def wide_resnet50_2_cifar100(pretrained=False, **kwargs):
-    model = models.wide_resnet50_2(pretrained=pretrained, num_classes=num_classes)
+    model = models.wide_resnet50_2(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
 @register_model
 def wide_resnet101_2_cifar100(pretrained=False, **kwargs):
-    model = models.wide_resnet101_2(pretrained=pretrained, num_classes=num_classes)
+    model = models.wide_resnet101_2(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return model
 
@@ -146,20 +144,20 @@ def resnext101_64x4d_cifar100(pretrained=False, **kwargs):
 # ConvNeXt
 @register_model
 def convnext_base_cifar100(pretrained=False, **kwargs):
-    model = models.convnext_base(pretrained=pretrained, num_classes=num_classes)
+    model = models.convnext_base(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def convnext_large_cifar100(pretrained=False, **kwargs):
-    model = models.convnext_large(pretrained=pretrained, num_classes=num_classes)
+    model = models.convnext_large(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def convnext_small_cifar100(pretrained=False, **kwargs):
-    model = models.convnext_small(pretrained=pretrained, num_classes=num_classes)
+    model = models.convnext_small(pretrained=pretrained, num_classes=100)
     return model
 
 @register_model
 def convnext_tiny_cifar100(pretrained=False, **kwargs):
-    model = models.convnext_tiny(pretrained=pretrained, num_classes=num_classes)
+    model = models.convnext_tiny(pretrained=pretrained, num_classes=100)
     return model
--- models/cifar100_lp.py
+++ models/cifar100_lp.py
@@ -4,123 +4,121 @@ import torchvision.models as models
 from timm.models.registry import register_model
 from .lpconv import LpConvert
 
-num_classes = 100
-
 # AlexNet
 @register_model
 def lp_alexnet_cifar100(pretrained=False, **kwargs):
-    model = models.alexnet(pretrained=pretrained, num_classes=num_classes)
+    model = models.alexnet(pretrained=pretrained, num_classes=100)
     model.features[0] = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 # VGG
 @register_model
 def lp_vgg11_cifar100(pretrained=False, **kwargs):
-    model = models.vgg11(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg11(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_vgg13_cifar100(pretrained=False, **kwargs):
-    model = models.vgg13(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg13(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_vgg16_cifar100(pretrained=False, **kwargs):
-    model = models.vgg16(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg16(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_vgg19_cifar100(pretrained=False, **kwargs):
-    model = models.vgg19(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg19(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 # VGG + BN
 @register_model
 def lp_vgg11_bn_cifar100(pretrained=False, **kwargs):
-    model = models.vgg11_bn(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg11_bn(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_vgg13_bn_cifar100(pretrained=False, **kwargs):
-    model = models.vgg13_bn(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg13_bn(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_vgg16_bn_cifar100(pretrained=False, **kwargs):
-    model = models.vgg16_bn(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg16_bn(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_vgg19_bn_cifar100(pretrained=False, **kwargs):
-    model = models.vgg19_bn(pretrained=pretrained, num_classes=num_classes)
+    model = models.vgg19_bn(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 # ResNet
 @register_model
 def lp_resnet18_cifar100(pretrained=False, **kwargs):
-    model = models.resnet18(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet18(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_resnet34_cifar100(pretrained=False, **kwargs):
-    model = models.resnet34(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet34(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_resnet50_cifar100(pretrained=False, **kwargs):
-    model = models.resnet50(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet50(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_resnet101_cifar100(pretrained=False, **kwargs):
-    model = models.resnet101(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet101(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_resnet152_cifar100(pretrained=False, **kwargs):
-    model = models.resnet152(pretrained=pretrained, num_classes=num_classes)
+    model = models.resnet152(pretrained=pretrained, num_classes=100)
     model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 # DenseNet
 @register_model
 def lp_densenet121_cifar100(pretrained=False, **kwargs):
-    model = models.densenet121(pretrained=pretrained, num_classes=num_classes)
+    model = models.densenet121(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_densenet161_cifar100(pretrained=False, **kwargs):
-    model = models.densenet161(pretrained=pretrained, num_classes=num_classes)
+    model = models.densenet161(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 96, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_densenet169_cifar100(pretrained=False, **kwargs):
-    model = models.densenet169(pretrained=pretrained, num_classes=num_classes)
+    model = models.densenet169(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_densenet201_cifar100(pretrained=False, **kwargs):
-    model = models.densenet201(pretrained=pretrained, num_classes=num_classes)
+    model = models.densenet201(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 # WideResNet
 @register_model
 def lp_wide_resnet50_2_cifar100(pretrained=False, **kwargs):
-    model = models.wide_resnet50_2(pretrained=pretrained, num_classes=num_classes)
+    model = models.wide_resnet50_2(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_wide_resnet101_2_cifar100(pretrained=False, **kwargs):
-    model = models.wide_resnet101_2(pretrained=pretrained, num_classes=num_classes)
+    model = models.wide_resnet101_2(pretrained=pretrained, num_classes=100)
     model.features.conv0 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False) # for cifar shape 32x32
     return LpConvert(model, log2p=4)
 
@@ -146,20 +144,20 @@ def lp_resnext101_64x4d_cifar100(pretrained=False, **kwargs):
 # ConvNeXt
 @register_model
 def lp_convnext_base_cifar100(pretrained=False, **kwargs):
-    model = models.convnext_base(pretrained=pretrained, num_classes=num_classes)
+    model = models.convnext_base(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_convnext_large_cifar100(pretrained=False, **kwargs):
-    model = models.convnext_large(pretrained=pretrained, num_classes=num_classes)
+    model = models.convnext_large(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_convnext_small_cifar100(pretrained=False, **kwargs):
-    model = models.convnext_small(pretrained=pretrained, num_classes=num_classes)
+    model = models.convnext_small(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
 
 @register_model
 def lp_convnext_tiny_cifar100(pretrained=False, **kwargs):
-    model = models.convnext_tiny(pretrained=pretrained, num_classes=num_classes)
+    model = models.convnext_tiny(pretrained=pretrained, num_classes=100)
     return LpConvert(model, log2p=4)
