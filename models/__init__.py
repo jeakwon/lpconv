563bfc69999ae2e327ee34bd9422021cd74ad412
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-06-23 21:42:42 +0900

    update

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,9 @@ from .lpconv import *
 from .cifar100 import *
 from .cifar100_lp import *
 from .cifar100_lp2 import *
+from .cifar100_lp2a import *
+from .cifar100_lp2b import *
+from .cifar100_lp2c import *
 from .imnet200 import *
 from .imnet200_lp import *
 from .imnet200_lp2 import *
