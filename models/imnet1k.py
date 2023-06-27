6a6f1d121b5b5fceac7ee9d0e170ef4b750407e3
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-06-28 01:30:03 +0900

    print

--- models/imnet1k.py
+++ models/imnet1k.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 from timm.models.registry import register_model
-from .lpconv import LpConvert
+from .lpconv2 import LpConvert
 
 @register_model
 def alexnet_imnet1k(pretrained=True, **kwargs):
@@ -11,6 +11,7 @@ def alexnet_imnet1k(pretrained=True, **kwargs):
     "acc@5": 79.066,
     """
     path = "/mnt/lustre/ibs/jeakwon/pretrained_models/alexnet.pth"
+    print(path)
     weights = torch.load(path)
     model = models.alexnet(weights=weights)
     return model
@@ -22,6 +23,7 @@ def vgg16_bn_imnet1k(pretrained=True, **kwargs):
     "acc@5": 91.516,
     """
     path = "/mnt/lustre/ibs/jeakwon/pretrained_models/vgg16_bn.pth"
+    print(path)
     weights = torch.load(path)
     model = models.vgg16_bn(weights=weights)
     return model
@@ -33,6 +35,7 @@ def resnet18_imnet1k(pretrained=True, **kwargs):
     "acc@5": 89.078,
     """
     path = "/mnt/lustre/ibs/jeakwon/pretrained_models/resnet18.pth"
+    print(path)
     weights = torch.load(path)
     model = models.resnet18(weights=weights)
     return model
@@ -44,6 +47,7 @@ def resnet34_imnet1k(pretrained=True, **kwargs):
     "acc@5": 91.420,
     """
     path = "/mnt/lustre/ibs/jeakwon/pretrained_models/resnet34.pth"
+    print(path)
     weights = torch.load(path)
     model = models.resnet34(weights=weights)
     return model
@@ -55,6 +59,7 @@ def resnet50_imnet1k(pretrained=True, **kwargs):
     "acc@5": 95.434,
     """
     path = "/mnt/lustre/ibs/jeakwon/pretrained_models/resnet50.pth"
+    print(path)
     weights = torch.load(path)
     model = models.resnet50(weights=weights)
     return model
@@ -66,6 +71,7 @@ def resnext50_32x4d_imnet1k(pretrained=True, **kwargs):
     "acc@5": 93.698,
     """
     path = "/mnt/lustre/ibs/jeakwon/pretrained_models/resnext50_32x4d.pth"
+    print(path)
     weights = torch.load(path)
     model = models.resnext50_32x4d(weights=weights)
     return model
@@ -77,6 +83,7 @@ def densenet121_imnet1k(pretrained=True, **kwargs):
     "acc@5": 91.972,
     """
     path = "/mnt/lustre/ibs/jeakwon/pretrained_models/densenet121.pth"
+    print(path)
     weights = torch.load(path)
     model = models.densenet121(weights=weights)
     return model
@@ -88,6 +95,7 @@ def convnext_base_imnet1k(pretrained=True, **kwargs):
     "acc@5": 96.870,
     """
     path = "/mnt/lustre/ibs/jeakwon/pretrained_models/convnext_base.pth"
+    print(path)
     weights = torch.load(path)
     model = models.convnext_base(weights=weights)
     return model
