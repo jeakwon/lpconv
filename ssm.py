ebe2a43ba64e9d2f52362dc6c864ec28aad91396
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-08-25 13:07:38 +0900

    update

--- ssm.py
+++ ssm.py
@@ -3,5 +3,7 @@ import models
 
 def load_checkpoint(path_to_checkpoint):
     checkpoint = torch.load(path_to_checkpoint, map_location='cpu')
-    model = getattr(models, checkpoint['args'].model).load_state_dict(checkpoint['model'])
+    args = checkpoint['args']
+    model = getattr(models, args.model)(num_classes=args.nb_classes)
+    model.load_state_dict(checkpoint['model'])
     return model
