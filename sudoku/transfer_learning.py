e62fcf9cc8a63d4660ae161e95f892e12def9d95
jeakwon <33548421+jeakwon@users.noreply.github.com>
2023-09-22 22:32:59 +0900

    update

--- sudoku/transfer_learning.py
+++ sudoku/transfer_learning.py
@@ -57,8 +57,8 @@ if __name__ == "__main__":
             except Exception as e: 
                 print(e)
                 
-
-        benchmark(new_model, save_dir=args.save_dir, data_path=args.data_path, batch_size=args.batch_size, lr=args.lr, seed=exp_args.seed, verbose=args.verbose, debug=args.debug)
+        save_dir = os.path.join(args.save_dir, exp_args.seed)
+        benchmark(new_model, save_dir=save_dir, data_path=args.data_path, batch_size=args.batch_size, lr=args.lr, seed=exp_args.seed, verbose=args.verbose, debug=args.debug)
         # for name, p in new_model.named_parameters():
         #     print(p)
 
